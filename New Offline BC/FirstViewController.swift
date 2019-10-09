1f08cc2aa940670c37dc8847d016077023ab5fde
Antonio <j.antonio.oro@gmail.com>
2019-10-09 16:52:39 -0500

    Initial Commit

---
+++ New Offline BC/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  New Offline BC
+//
+//  Created by Antonio Orozco on 10/9/19.
+//  Copyright © 2019 Antonio Orozco. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ New Offline BC/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  New Offline BC
+//
+//  Created by Antonio Orozco on 10/9/19.
+//  Copyright © 2019 Antonio Orozco. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
